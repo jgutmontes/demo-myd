50d7799c6b836a41bf272270ad1259c21433eda6
jgutmontes <janet.gutierrez.montalban@gmail.com>
2025-09-03 17:16:37 +0000

    ffeat:myd index

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -68,7 +68,7 @@ export const experts: Expert[] = [
   {
     id: '1',
     name: 'Lizzeth Miranda Díaz',
-    title: 'Especialista de postcosecha en arándanos y paltas. Gerente de Calidad',
+    title: 'Especialista de postcosecha en arándanos y paltas. Gerente de Calidadd',
     image: 'https://images.unsplash.com/photo-1498557850523-fd3d118b962e?w=400&h=300&fit=crop',
     specialization: 'Postcosecha'
   },
